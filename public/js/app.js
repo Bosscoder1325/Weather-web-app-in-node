85d5366a26e0c81adfee31e71e8e2c33fc09e7f1
bossCoder <bosscoder1325@gmail.com>
2021-08-28 07:22:58 +0530

    change loading line

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = ""
   messageTwo.textContent = ""
-  messageOne.textContent = `Loading weather for ${location} ...`;
+  messageOne.textContent = `Loading weather for ${location} ....`;
   fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
